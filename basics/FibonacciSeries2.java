cc1a97052f86ea1cca3ccc0e265de69d5e36496e
Sarika Mehra <mehrasarika574@gmail.com>
2026-05-19 12:57:23 +0530

    Added Tabulation method

--- basics/FibonacciSeries2.java
+++ basics/FibonacciSeries2.java
@@ -1,11 +1,11 @@
 package basics;
 import java.util.*;
 
-import java.util.Scanner;
+public class FibonacciSeries2 {
 
-// This is the memoization technique used in dp 
-
-public class FabonacciSeries {
+    // This is the tabulation technique used in dp where the 
+    //TC --> O(N)
+    //SC --> O(1)
 
     static int f(int n, int[] dp){
 
@@ -21,15 +21,30 @@ public class FabonacciSeries {
 
         return dp[n] = f(n - 1, dp) + f(n - 2, dp);
     }
+
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // Initializing the dp array
+         // Initializing the dp array
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
 
+        int prev = 1 ;
+        int prev2 = 0;
+
+        for(int i = 2 ; i < n ; i++){
+            int curr = prev + prev2;
+
+            prev2 = prev ;
+            prev = curr;
+        }
+        
         System.out.println(f(n, dp));
+       
+        
     }
     
 }
