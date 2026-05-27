8ba8b1a264f2abd5dedc19b67c8497985d16574f
Sarika Mehra <mehrasarika574@gmail.com>
2026-05-27 21:13:03 +0530

    follow up added

---
+++ basics/KFrogJump.java
@@ -0,0 +1,35 @@
+package basics;
+import java.util.*;
+
+public class KFrogJump {
+
+    public static int kJumps(int n, int[] heights, int[] dp, int k){
+        if(n == 0) return 0;
+        
+        if(dp[n] != -1) return dp[n];
+        int minEnergy = Integer.MAX_VALUE;
+        for(int j = 1 ; j <= k ; j++){
+            if(n - j >= 0){
+                int energy = kJumps(n - j, heights, dp, k) + Math.abs(heights[n] - heights[n - j]);
+                minEnergy = Math.min(minEnergy, energy);
+            }
+        }
+        dp[n] = minEnergy;
+        return minEnergy;
+    }
+
+
+    public static void main(String[] args) {
+        int[] heights = {30, 10, 60, 10, 60, 50};
+        int n = heights.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        int k = 2;
+        System.out.println(kJumps(n - 1, heights, dp, k));
+
+
+    }
+    
+}
